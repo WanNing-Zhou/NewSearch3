f937f2d79a58301cc38f23bfe8b196460229e3ab
WangNing-Zhou <1879154660@qq.com>
2023-08-04 17:09:32 +0800

    feat(dialog): 添加弹出框组件

---
+++ src/components/WQDialog/WQDialog.vue
@@ -0,0 +1,59 @@
+/*  *
+  * @FileNAme src\components\WQDialog\WQDialog.vue
+  * @author 周万宁
+  * @create 2023/8/4-16:42
+  * @version
+  * @description 弹出框组件
+  */
+
+<template>
+  <div class="wq-dialog" v-show="visible">
+    <div class="wq-dialog-overlay" @click="closeDialog"></div>
+
+    <div class="wq-dialog-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+const props = defineProps({
+  visible:Boolean, //dialog 可见性
+})
+
+const emit = defineEmits(['close'])
+
+// 弹出框关闭方法
+const closeDialog = ()=>{
+  emit('close')
+}
+
+</script>
+
+<style lang="scss" scoped>
+.wq-dialog{
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  .wq-dialog-overlay{
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: -1;
+  }
+  .wq-dialog-content {
+    background-color: #fff;
+    padding: 2rem;
+  }
+}
+</style>
--- src/views/TestView/index.vue
+++ src/views/TestView/index.vue
@@ -1,37 +1,25 @@
 <template>
   <div>
-    <swiper :modules="modules" :navigation="true" :loop="true" :pagination="{ clickable: true }">
-      <!-- modules为导入的模块，绑定导入的模块导航模块和分页模块，loop实现轮播图循环模式 -->
-      <swiper-slide>
-        <img src="/imgs/testSwiper/img1.jpg">
-      </swiper-slide>
-      <swiper-slide>
-        <img src="/imgs/testSwiper/img2.jpg">
-
-      </swiper-slide>
-      <swiper-slide>
-        <img src="/imgs/testSwiper/img3.jpg">
-      </swiper-slide>
-
-    </swiper>
-
+    <WQDialog @close="dialogClose" :visible="dialogVisible">
+      <div>这是一个测试程序</div>
+    </WQDialog>
   </div>
 </template>
 
-<script lang="ts" setup>
-// 这里需要引入组件
-import { Swiper, SwiperSlide } from 'swiper/vue';
-// 这里需要引入模块
-//pagination 就是指示器, 在效果中为小圆点...
-// 他有很多的模块, 具体看官方文档
-import { Pagination,Navigation } from 'swiper/modules';
-import {ref} from "vue";
-// 这里需要引入样式,
-import 'swiper/css/pagination';
-import 'swiper/css/navigation';
-import 'swiper/css';
+<script setup lang="ts">
 
-const modules = ref([Navigation,Pagination])
+import WQDialog from "@/components/WQDialog/WQDialog.vue";
+import {ref} from "vue";
+
+const dialogVisible = ref(true)
+
+const dialogClose = ()=>{
+  console.log('关闭程序触发了')
+  dialogVisible.value = false;
+}
 
 </script>
 
+<style scoped>
+
+</style>
