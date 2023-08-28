63d84fc65df9d29fbbcc4da1066d1e9464cb86e9
WangNing-Zhou <1879154660@qq.com>
2023-08-28 23:41:50 +0800

    build

--- src/store/useStore.ts
+++ src/store/useStore.ts
@@ -2,10 +2,12 @@ import {useHomeStore} from "@/store/homeStore.ts";
 import {useUserStore} from "@/store/useUserStore.ts"
 import {searchStore} from "@/store/searchStore.ts";
 import {componentsVisibleStore} from '@/store/componentsVisible.ts'
+import {configStore} from "@/store/config.ts";
 
 export default {
     useHomeStore,
     useUserStore,
     searchStore,
     componentsVisibleStore,
+    configStore,
 }
