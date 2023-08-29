78c46f19dadc69090a5f879124076c28dd0c19f0
WangNing-Zhou <1879154660@qq.com>
2023-08-30 00:01:01 +0800

    feat: 添加设置菜单栏

--- src/store/useUserStore.ts
+++ src/store/useUserStore.ts
@@ -9,3 +9,4 @@ export const useUserStore = defineStore({
     getters: {},
     actions: {}
 })
+
--- src/views/Home/components/PageConfig/PageConfig.vue
+++ src/views/Home/components/PageConfig/PageConfig.vue
@@ -12,21 +12,40 @@
           margin: 0,
           left: '60px',
           bottom: '70px',
-          width: '400px',
           position: 'absolute'
         }"
         v-model:visible="dialogVisible">
 
       <el-container>
-        <el-aside width="80px">
-          你好
+        <el-button @click="isCollapse = !isCollapse">开关</el-button>
+      <div class="page-aside">
+        <el-aside>
+          <el-menu
+              default-active="1"
+              :collapse="isCollapse"
+          >
+            <el-menu-item index="4">
+              <el-icon><setting /></el-icon>
+              <template #title>Navigator Four</template>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <el-icon><setting /></el-icon>
+              <template #title>Navigator Four</template>
+            </el-menu-item>
+            <el-menu-item index="4">
+              <el-icon><setting /></el-icon>
+              <template #title>Navigator Four</template>
+            </el-menu-item>
+
+          </el-menu>
         </el-aside>
+      </div>
+
         <el-main>
           这是一个简单的语句
         </el-main>
       </el-container>
     </WQDialog0>
-
   </div>
 
 </template>
@@ -37,9 +56,11 @@ import GroundGlass from "@/components/GroundGlass/GroundGlass.vue";
 
 import {ref} from "vue";
 import WQDialog0 from "@/components/WQDialog0/WQDialog0.vue";
+import SvgIcon from "@/components/SvgIcon/SvgIcon.vue";
 
 const dialogVisible = ref(false)
-
+// 用于控制菜单栏可见性
+const isCollapse = ref(false)
 </script>
 
 <style lang="scss" scoped>
@@ -58,6 +79,16 @@ const dialogVisible = ref(false)
 
   &:hover {
     box-shadow: 0 0 3px rgba(255, 255, 255, 0.6);
+  }
+}
+
+
+.page-aside{
+
+  :deep(.el-aside){
+    width: 0;
+    display: inline-block;
+    max-height: 500px;
   }
 }
 
